eea0ffa485e820475a2d7bff943915d6e47bdcf5
DallonF <dallon.feldner@gmail.com>
2018-01-27 09:13:03 -0600

    Lighting post process effect

---
+++ Assets/Scripts/LightingPostProcessEffect.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightingPostProcessEffect : MonoBehaviour
+{
+
+	public Material Material;
+	public RenderTexture renderTexture;
+	public Camera lightingCamera;
+
+	private void Awake()
+	{
+		if (!renderTexture)
+		{
+			renderTexture = new RenderTexture(
+				Screen.width,
+				Screen.height,
+				0,
+				RenderTextureFormat.RG32,
+				RenderTextureReadWrite.Default
+			);
+			Material.SetTexture("_Lighting", renderTexture);
+		}
+	}
+
+	private void Start() {
+		lightingCamera.enabled = true;
+		lightingCamera.targetTexture = renderTexture;
+	}
+
+	private void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		Graphics.Blit(src, dest, Material);
+	}
+}
