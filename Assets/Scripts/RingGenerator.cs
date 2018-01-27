22d912cac50e746003b0486ddf3e7bc2d69e4733
DallonF <dallon.feldner@gmail.com>
2018-01-27 15:00:54 -0600

    Start shockwave/shout effect

---
+++ Assets/Scripts/RingGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(LineRenderer))]
+public class RingGenerator : MonoBehaviour
+{
+	public float Radius = 1;
+	public int Segments = 8;
+	private LineRenderer lineRenderer;
+
+	void Awake()
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+	}
+
+	void Start()
+	{
+		lineRenderer.positionCount = Segments;
+		var positions = new Vector3[lineRenderer.positionCount];
+		var fullCircle = Mathf.PI * 2;
+		for (int i = 0; i < lineRenderer.positionCount; i++)
+		{
+			var index = i % Segments;
+			var percentOfCircle = (float)index / Segments;
+			var radians = fullCircle * percentOfCircle;
+			positions[i] = new Vector3(Mathf.Sin(radians), Mathf.Cos(radians), 0);
+		}
+		lineRenderer.SetPositions(positions);
+	}
+
+	void Update()
+	{
+
+	}
+}
