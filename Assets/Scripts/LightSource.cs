d16f125ad42fe37537d981ac1b3e89ec6965610d
DallonF <dallon.feldner@gmail.com>
2018-01-27 09:56:32 -0600

    LightSource component

---
+++ Assets/Scripts/LightSource.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSource : MonoBehaviour
+{
+	public SpriteRenderer LightmaskPrefab;
+	public float Radius = 0;
+	public float Opacity = 0;
+
+	private SpriteRenderer lightmask;
+
+	// Use this for initialization
+	void Start()
+	{
+		lightmask = GameObject.Instantiate(
+			LightmaskPrefab,
+			transform.position,
+			Quaternion.identity
+		).GetComponent<SpriteRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		lightmask.transform.localScale = Vector3.one * Radius;
+		lightmask.color = new Color(1, 1, 1, Opacity);
+	}
+}
---
+++ Assets/WorkingScenes/Dallon/DallonWorkingScene002-Lighting/TestStar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(LightSource))]
+public class TestStar : MonoBehaviour {
+
+	public float MinTime = 1;
+	public float MaxTime = 5;
+	public float MinRadius = 1;
+	public float MaxRadius = 5;
+
+	public float DecaySpeed = 0.1f;
+
+	private LightSource lightSource;
+
+	private void Awake() {
+		lightSource = GetComponent<LightSource>();
+	}
+
+	void Start () {
+		StartCoroutine(Twinkle());	
+	}
+	IEnumerator Twinkle() {
+		while(true) {
+			yield return new WaitForSeconds(Random.Range(MinTime,  MaxTime));
+			lightSource.Radius = Random.Range(MinRadius, MaxRadius);
+			while  (lightSource.Radius > 0.01) {
+				yield return null;
+				lightSource.Radius -= DecaySpeed * Time.deltaTime;
+			}
+			lightSource.Radius = 0;
+		}
+	}
+}
