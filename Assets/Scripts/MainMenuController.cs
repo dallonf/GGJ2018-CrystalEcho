68de09cc792f215059320ab80eecb6015db99b25
CWNichols01 <cwnichols01@gmail.com>
2018-01-27 05:27:25 -0600

    Updates to main menu controller and added tower controller.

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -1,16 +1,58 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour {
 
+	public GameObject controlsBackground;
+	public GameObject creditsBackground;
+	public GameObject quitBackground;
+
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void StartButton()
+	{
+		SceneManager.LoadScene("GameScene");
+	}
+
+	public void ControlsButton()
+	{
+		controlsBackground.SetActive (true);
+	}
+
+	public void CreditsButton()
+	{
+		creditsBackground.SetActive (true);
+	}
+
+	public void QuitButton()
+	{
+		quitBackground.SetActive (true);
+	}
+
+	public void ControlsBackButton ()
+	{
+		controlsBackground.SetActive (false);
+	}
+
+	public void CreditsBackButton ()
+	{
+		creditsBackground.SetActive (false);
+	}
+
+	public void QuitNoButton ()
+	{
+		quitBackground.SetActive (false);
+	}
+
+	public void QuitYesButton ()
+	{
+		Application.Quit ();
 	}
 }
