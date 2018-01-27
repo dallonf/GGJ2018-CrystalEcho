7f498c3ac544cfb5e0129b031587b8b872446620
CWNichols01 <cwnichols01@gmail.com>
2018-01-27 00:18:22 -0600

    Unified Dallon and Clarence projects.

---
+++ Assets/Scripts/PlayerMover.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMover : MonoBehaviour {
+
+	float speed = 2.0f;
+
+	Vector3 movement;
+
+	void Start () 
+	{
+		movement = new Vector3 ();
+	}
+
+	void Update ()
+	{
+		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow))
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+			movement += Vector3.up;
+		}
+
+		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			transform.position += Vector3.left * speed * Time.deltaTime;
+			movement += Vector3.left;
+		}
+
+		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) 
+		{
+			transform.position += Vector3.down * speed * Time.deltaTime;
+			movement += Vector3.down;
+		}
+
+		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.position += Vector3.right * speed * Time.deltaTime;
+			movement += Vector3.right;
+		}
+
+		movement = movement.normalized;
+
+//		transform.rotation = Quaternion.LookRotation(movement);
+
+		transform.rotation = Quaternion.Euler(movement);
+	}
+}
