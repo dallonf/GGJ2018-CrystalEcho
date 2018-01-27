bb143cf041c9788ca4187484396187bc5d701cb4
DallonF <dallon.feldner@gmail.com>
2018-01-26 23:34:51 -0600

    Pong arrow now functional

---
+++ Assets/Scripts/PongArrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PongArrow : MonoBehaviour
+{
+	public Transform PointAt;
+	public Animator Animator;
+	public string DoneState = "done";
+
+	// Update is called once per frame
+	void Update()
+	{
+		var angle = Mathf.Atan2(
+			transform.position.y - PointAt.position.y,
+			transform.position.x - PointAt.position.x
+		) * Mathf.Rad2Deg + 90;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		if (Animator.GetCurrentAnimatorStateInfo(0).IsName(DoneState))
+		{
+			GameObject.Destroy(gameObject);
+		}
+	}
+}
