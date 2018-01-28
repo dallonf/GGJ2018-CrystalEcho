677200cd24dce466e6bcbc795c753fed05058828
DallonF <dallon.feldner@gmail.com>
2018-01-27 23:09:21 -0600

    Buttons can now ping objects

--- Assets/Scripts/NodeListElement.cs
+++ Assets/Scripts/NodeListElement.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class NodeListElement : MonoBehaviour
+{
+	public KnowledgeOwner KnowledgeOwner;
+	public KnowableObject KnowableObject;
+	public Text text;
 
-	// Use this for initialization
-	void Start () {
-		
+	public void InitObject(KnowableObject obj)
+	{
+		KnowableObject = obj;
+		text.text = obj.name;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void Ping()
+	{
+		ExecuteEvents.Execute<IPingable>(KnowableObject.gameObject, null, (x, y) => x.Ping(KnowledgeOwner));
 	}
 }
