004191b5eb9703b2fc892f35f8d766d4fbe82f4c
DallonF <dallon.feldner@gmail.com>
2018-01-28 14:05:44 -0600

    Start EndGameArea script

---
+++ Assets/Scripts/EndGameArea.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class EndGameArea : MonoBehaviour {
+
+	public float FinishRadius = 1f;
+	private GameObject trackingPlayer;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Player")) {
+
+		}
+	}
+}
