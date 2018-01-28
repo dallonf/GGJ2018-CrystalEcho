6c353ce7b2637c381902176af85e73fccfb8c3e6
DallonF <dallon.feldner@gmail.com>
2018-01-28 14:24:16 -0600

    Fade to exit works

--- Assets/Scripts/EndGameArea.cs
+++ Assets/Scripts/EndGameArea.cs
@@ -3,19 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider))]
-public class EndGameArea : MonoBehaviour {
+public class EndGameArea : MonoBehaviour
+{
 
 	public float FinishRadius = 1f;
+	public float StartRadius = 40;
+	public CanvasGroup FadeToWhite;
 	private GameObject trackingPlayer;
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update()
+	{
+		if (trackingPlayer)
+		{
+			var distance = Vector3.Distance(trackingPlayer.transform.position, transform.position);
+			float percent = Mathf.InverseLerp(StartRadius, FinishRadius, distance);
+			FadeToWhite.alpha = percent;
+		}
 	}
 
-	private void OnTriggerEnter(Collider other) {
-		if (other.CompareTag("Player")) {
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			trackingPlayer = other.gameObject;
+			FadeToWhite.gameObject.SetActive(true);
+		}
+	}
 
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.gameObject == trackingPlayer)
+		{
+			trackingPlayer = null;
+			FadeToWhite.gameObject.SetActive(false);
 		}
 	}
 }
