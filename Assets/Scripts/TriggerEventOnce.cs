99d90c8e8f470649c2718f7258a4bcd500ec3669
DallonF <dallon.feldner@gmail.com>
2018-01-28 13:19:45 -0600

    Only trigger story messages once

---
+++ Assets/Scripts/TriggerEventOnce.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerEventOnce : MonoBehaviour
+{
+  public UnityEvent Event;
+  public bool HasBeenTriggered = false;
+
+  public void Trigger()
+  {
+    if (!HasBeenTriggered) {
+    Event.Invoke();
+    HasBeenTriggered = true;
+    }
+  }
+}
