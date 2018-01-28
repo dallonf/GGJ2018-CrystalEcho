70a1f01f91dcd9e0e2ebebf772ec2b62d36b84db
DallonF <dallon.feldner@gmail.com>
2018-01-28 11:36:23 -0600

    + player does not go through walls - player does not ever stop

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -2,50 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMover : MonoBehaviour {
-
+// [RequireComponent(typeof(Rigidbody))]
+public class PlayerMover : MonoBehaviour 
+{
+	private Rigidbody rigidbody;
 	public float speed = 2.0f;
 
-	Vector3 movement;
+	private void Awake() 
+	{
+		rigidbody = GetComponent<Rigidbody>();	
+	}
 
 	void Start () 
 	{
-		movement = new Vector3 ();
 	}
 
 	void Update ()
 	{
+		Vector3 movement = Vector3.zero;
 		if (Input.GetKey (KeyCode.W) || Input.GetKey (KeyCode.UpArrow))
 		{
-			transform.position += Vector3.up * speed * Time.deltaTime;
+			// transform.position += Vector3.up * speed * Time.deltaTime;
 			movement += Vector3.up;
 		}
 
 		if (Input.GetKey (KeyCode.A) || Input.GetKey (KeyCode.LeftArrow)) 
 		{
-			transform.position += Vector3.left * speed * Time.deltaTime;
+			// transform.position += Vector3.left * speed * Time.deltaTime;
 			movement += Vector3.left;
 		}
 
 		if (Input.GetKey (KeyCode.S) || Input.GetKey (KeyCode.DownArrow)) 
 		{
-			transform.position += Vector3.down * speed * Time.deltaTime;
+			// transform.position += Vector3.down * speed * Time.deltaTime;
 			movement += Vector3.down;
 		}
 
 		if (Input.GetKey (KeyCode.D) || Input.GetKey (KeyCode.RightArrow)) 
 		{
-			transform.position += Vector3.right * speed * Time.deltaTime;
+			// transform.position += Vector3.right * speed * Time.deltaTime;
 			movement += Vector3.right;
 		}
+		movement.Normalize();
 
-		movement = movement.normalized;
+		if (movement.magnitude > Mathf.Epsilon) {
+			rigidbody.velocity = movement * speed;
+		}
 
 		var angle = Mathf.Atan2(
 			movement.y,
 			movement.x
 		) * Mathf.Rad2Deg - 90;
 
-		transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), speed / 20);
+		transform.rotation = Quaternion.Slerp (
+			transform.rotation,
+			Quaternion.AngleAxis(angle, Vector3.forward),
+			speed / 20
+		);
 	}
 }
